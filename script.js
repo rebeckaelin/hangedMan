8560e4146a6b28724f0cc2e5ef601c8f39c403f3
rebeckaelin <rebeckaelin@hotmail.com>
2023-11-06 17:53:15 +0100

    small changes

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ let usedLetters = document.querySelector('.used-letters')
 let showWord = document.querySelector('.word')
 let guessButton = document.querySelector('#btn')
 
-let guessedLetter = [];
+// let guessedLetter = [];
+let userGuess;
 
 let levelOneWords = ["bil", "hund", "cykel", "sol", "blomma", "bok", "dator", "äpple", "katt", "regn", "glas", "stol", "sten", "måne", "ballong", "elefant", "flaska", "stjärna", "kaffe", "fågel", "träd", "fjäril", "buss", "gräs", "orm", "hus", "spegel", "flod", "mus", "nyckel", "fjäll", "korv", "park", "penna", "lampa", "toalett", "sko", "tåg", "fjäder", "musik", "vatten", "regnbåge", "kamera"]
 
@@ -18,7 +19,6 @@ typeOutWord = pickedWord.split('').fill('_',0).join(' ')
 showWord.innerHTML = typeOutWord //Skriver ut rätt antal _ baserat på ordet som slumpats
 
 // hämtar in det användaren skriver i inputfältet. sparas i en variabel
-let userGuess;
 guessButton.addEventListener('click', () => {
     userGuess = document.querySelector('#input').value;
     document.querySelector('#input').value = '';
@@ -26,17 +26,17 @@ guessButton.addEventListener('click', () => {
     
 
     if (pickedWord.includes(userGuess)) {
-        let typeOutArray = typeOutWord.split(' ');
+        let guessedLetter = typeOutWord.split(' ');
         
 
         for (let i = 0; i < pickedWord.length; i++) {
             if (pickedWord[i] === userGuess) {
-                typeOutArray[i] = userGuess;
+                guessedLetter[i] = userGuess;
             }
         }
-        typeOutWord = typeOutArray.join(' ')
+        typeOutWord = guessedLetter.join(' ')
         showWord.innerHTML = typeOutWord;
-    }else{
+    } else{
         guessedLetter.push(userGuess);
         usedLetters.innerHTML = guessedLetter
         console.log(guessedLetter);
