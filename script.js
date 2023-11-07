da4d1d0e25f04ea0fbbe461e3650cd7f8fafa880
cozyhorse <mo.apiban@gmail.com>
2023-11-07 11:10:03 +0100

    added more text to alert()

--- script.js
+++ script.js
@@ -9,11 +9,11 @@ const inputletter = document.querySelector("#guessletter");
 const printwords = document.querySelector("#score");
 const printguesses = document.querySelector("#tries");
 
-const words = ["motivation", "hund", "katt"];
+const words = ["motivation", "length", "brick", "tenant", "day", "gold", "technique", "sacred", "sunshine", "aquarium", "therapist", "jail", "technology", "javascript", "master", "surf", "electronics"];
 
 let gameWord = "";
 let failCount = 0;
-let wordtoShow = [];
+let wordToShow = [];
 let guessedWords = [];
 
 //Draw random word from array and return it
@@ -28,11 +28,13 @@ const initGame = () => {
     //Create a new array using Array object and set its length to gameWords length.
     //new Array creates an empty array by itself and by passing in gameWord.length it gets a length but doesn't have content
     //With fill("_") we can fill the array with "_" based on the length and generate diffrent length based on words it gets.
-  wordtoShow = new Array(gameWord.length).fill("_")
+  wordToShow = new Array(gameWord.length).fill("_")
   console.log("Word to guess: ", gameWord);
-  console.log("playfield: ", wordtoShow);
-  console.log("word length: ", wordtoShow.length);
-    printwords.textContent = wordtoShow.join(" ");
+  console.log("playfield: ", wordToShow);
+  console.log("word length: ", wordToShow.length);
+  //use join to make the letters in the array to one string and a separator.
+  //Then assign it to printWords.textContent to output on document
+    printwords.textContent = wordToShow.join(" ");
   findLetter();
 };
 
@@ -41,35 +43,40 @@ const initGame = () => {
 const findLetter = () => {
   guessbtn.addEventListener("click", () => {
     //assign inputletter"field" to guessletter
-    let guessletter = inputletter.value.toLocaleLowerCase();
+    let guessLetter = inputletter.value.toLocaleLowerCase();
 
-    console.log("letter guessed: ", guessletter);
+    console.log("letter guessed: ", guessLetter);
 
-    if(gameWord.includes(guessletter)) {
+    if(gameWord.includes(guessLetter)) {
         //Loops through "secret word"(gameWord)
       for (let index = 0; index < gameWord.length; index++) {
         //Checks the current array(gameWord) and checks if character at current index is strictly(===) guessletter
-        if (gameWord[index] === guessletter) {
-            //IF true check wordtoShow array and assign same index to guessletter
-          wordtoShow[index] = guessletter;
+        if (gameWord[index] === guessLetter) {
+            //IF true, check wordtoShow array and assign same index to guessletter
+          wordToShow[index] = guessLetter;
         }
       }
 
-      console.log("found words", wordtoShow);
-      printwords.textContent = wordtoShow.join(" ");
+      console.log("found words", wordToShow);
+      printwords.textContent = wordToShow.join(" ");
       inputletter.value = "";
 
-      if(!wordtoShow.includes("_")){
-        alert("Congratulations! You win!!!");
+    //Check if the wordtoShow does NOT include "_" to win the game.
+      if(!wordToShow.includes("_")){
+       setTimeout(()=> {
+        alert("Congratulations! You win!!!\nPress restart to play again!");
         guessbtn.disabled = true;
+
+       }, 100)
       }
 
     }else{
         //if letter not found, increment failCount
       failCount ++;
       console.log("Fail counter: ", failCount);
-      guessedWords.push(guessletter);
+      guessedWords.push(guessLetter);
       console.log("Words guessed: ", guessedWords);
+      //Print out guessed letters
       printguesses.textContent = guessedWords.join(" ")
       drawHangedMan()
       inputletter.value = "";
@@ -100,8 +107,8 @@ const drawHangedMan = () => {
         case 5:
             body.classList.remove("hide")
             setTimeout(() => {
-                alert("GAME OVER!!!")
-            }, 50)
+                alert(`GAME OVER!!!\nCorrect word was : ${gameWord}\nPress restart to play again.`)
+            }, 100)
             guessbtn.disabled = true
             break;
     }
